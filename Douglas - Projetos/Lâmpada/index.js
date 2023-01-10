ea41bdc20d780d1a6bec694be92ae04b71111e98
melodouglas97 <101734939+melodouglas97@users.noreply.github.com>
2023-01-10 12:46:58 -0300

    Add files via upload

---
+++ Douglas - Projetos/Lâmpada/index.js
@@ -0,0 +1,9 @@
+function apagar() {
+    document.querySelector('#lamp').classList.add('opacities');    
+}   
+
+function acender() {
+    document.querySelector('#lamp').classList.remove('opacities');
+    
+    
+}
